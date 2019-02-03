0e4d670d286734a6e54c0187240e913d91848768
Rotimi-Best <rotimiibitoyebest@yahoo.co.uk>
2019-02-03 21:33:25 +0200

    Installation setup

---
+++ database/migrations/2019_02_02_205551_create_tournaments_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTournamentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tournaments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->char('name', 100);
+            $table->char('country', 100);
+            $table->enum('level', ['rookie', 'amateur', 'pro']);
+            $table->integer('score');
+            $table->integer('age');
+            $table->boolean('suspected');
+            $table->timestamps('added_on');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tournaments');
+    }
+}
